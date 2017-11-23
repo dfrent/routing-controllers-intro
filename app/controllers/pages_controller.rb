0211988aec59dfc9846b9c20e4164702daa186f5
cordell <cordell.perry@gmail.com>
2017-11-23 09:40:16 -0500

    routes and instance variable set on @header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,24 @@
 class PagesController < ApplicationController
 
   def contest
-
+    @header = "good luck in the contest"
   end
 
+
   def welcome
+  @header = "This is the welcome page"
+  end
+
+  def kittens
+    @header = "1a 2b 3c 4d"
+  end
+
+  def secrets
+    @header = "ssssshhhhhhhhhhhhhh"
+  end
+
+  def about
+    @header = "this is a little story about us"
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ get '/' => 'pages#welcome'
 
 get '/welcome' => 'pages#welcome'
 
+get '/about' => 'pages#about'
+
 get '/contest' => 'pages#contest'
+
+get '/kittens' => 'pages#kittens'
+
+get 'secrets' => 'pages#secrets'
 
 end
